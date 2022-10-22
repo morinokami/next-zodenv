434b13b9f7ebe14fb460313ed871ad5978e9fb72
Shinya Fujino <shf0811@gmail.com>
2022-10-22 18:13:41 +0900

    Init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+}
