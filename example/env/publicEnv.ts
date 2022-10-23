33df2fa3d374fbbc4afdda71dddbd5d3ca3355fc
Shinya Fujino <shf0811@gmail.com>
2022-10-23 16:27:33 +0900

    Use nextPublic as a container for public values

--- example/env/publicEnv.ts
+++ example/env/publicEnv.ts
@@ -1,11 +1,13 @@
 import { zenv } from 'next-zodenv'
 import { z } from 'zod'
 
-// export const publicEnv = zenv(
-//   z.object({
-//     BAR: z.string(),
-//   }),
-//   { nextPublic: true },
-// )
-
-export const publicEnv = {}
+export const publicEnv = zenv(
+  z.object({
+    NEXT_PUBLIC_BAR: z.string(),
+  }),
+  {
+    nextPublic: {
+      NEXT_PUBLIC_BAR: process.env.NEXT_PUBLIC_BAR,
+    }
+  },
+)
--- package/src/types.ts
+++ package/src/types.ts
@@ -3,7 +3,7 @@ import { ZodError } from 'zod'
 export type ZodErrors = Record<string, ZodError>
 
 export type ZenvOptions = {
-  nextPublic?: boolean
+  nextPublic?: Record<string, unknown>
   env?: Record<string, string | undefined>
   reporter?: (errors: ZodErrors) => unknown
 }
