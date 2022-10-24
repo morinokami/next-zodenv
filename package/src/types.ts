efd4432e4cfc8edd6dd3ea5dcc58dfcd66ad2d23
Shinya Fujino <shf0811@gmail.com>
2022-10-25 01:21:11 +0900

    Update types.ts

--- package/src/types.ts
+++ package/src/types.ts
@@ -1,9 +1,11 @@
 import { ZodIssue } from 'zod'
 
+export type Environment = Record<string, string | undefined>
+
 export type ZodErrors = Record<string, ZodIssue[]>
 
 export type ZenvOptions = {
   nextPublic?: Record<string, unknown>
-  env?: Record<string, string | undefined>
+  env?: Environment
   reporter?: (errors: ZodErrors) => unknown
 }
