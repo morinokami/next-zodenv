810a8a1f36f7ffe76f97016168ee9488f8d57ed9
Shinya Fujino <shf0811@gmail.com>
2022-10-27 02:19:42 +0900

    Fix typo

--- examples/next13/pages/api/hello.ts
+++ examples/next13/pages/api/hello.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { serverEnv } from '../../env/serverenv'
+import { serverEnv } from '../../env/serverEnv'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(serverEnv)
